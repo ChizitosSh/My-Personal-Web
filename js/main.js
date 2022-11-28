fc1d39f66ed2b8905c3c2a420073654cf3baccf8
ChizitosSh <116261275+ChizitosSh@users.noreply.github.com>
2022-11-28 06:08:25 -0500

    Update main.js

---
+++ js/main.js
@@ -0,0 +1,17 @@
+// Scroll Reveal
+
+const navbar = document.getElementById('navbar');
+
+const scroll = ScrollReveal ({
+    origin: 'top',
+    distance: '50px',
+    duration: 1800, // milisegundos -> 2.5s
+    delay: 200, // delay -> retraso, para que no aparezca de golpe
+    reset: true, // para que se vuelva a ejecutar la animación
+})
+
+scroll.reveal(`.navbar-brand, .navbar-toggle, .col-sm-12, .scroll-top`)
+
+scroll.reveal(`.scroll-right`, {origin: 'right'})
+
+scroll.reveal(`.scroll-left`, {origin: 'left'})
